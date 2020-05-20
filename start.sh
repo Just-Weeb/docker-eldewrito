b983f7153bf286db41967c2f7b85f8668fcdb227
hkwebmaster <65623162+hkwebmaster@users.noreply.github.com>
2020-05-20 16:48:24 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -125,7 +125,7 @@ Xvfb :1 -screen 0 320x240x24 &
 echo "${GREEN}Starting dedicated server${NC}"
 
 # DLL overrides for Wine are required to prevent issues with master server announcement
-export WINEDLLOVERRIDES="winhttp,rasapi32=n;xinput1_3=n"
+export WINEDLLOVERRIDES="winhttp,rasapi32=n"
 
 if [ ! -z "${WINE_DEBUG}" ]; then
     echo "Setting wine to verbose output"
